e0b9bfc951e2ab853f472faad8b90f2a80437466
kaminese <56663339+kaminese@users.noreply.github.com>
2020-11-07 15:38:18 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,9 +5,40 @@ var options = {
     iconUrl: "logo2.png"
 };
 
+/* 
 chrome.notifications.create(options, callback);
 console.log('POP');
 
 function callback(){
     console.log('Popup Done!');
     }
+*/
+
+
+//Timer Function: 
+const startingMinutes = 1;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('countdown')
+
+let a = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+    if (countdownEl.innerHTML == '0:00') {
+        clearInterval(a)
+        chrome.notifications.create(options, callback);
+            console.log('POP');
+
+        function callback(){
+            console.log('Popup Done!');
+        }
+    }
+}
