f4498572479ae35bd09bc86ad54e6432145b3f49
kaminese <56663339+kaminese@users.noreply.github.com>
2020-11-07 18:00:45 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -9,42 +9,47 @@ function callback(){
     console.log('Popup Done!');
 }
 
-// 20 MINUTE WORKING TIME Function:
-const startingMinutes = 1;
-let time = startingMinutes * 60; // 1 minute * 60 seconds
-const countdownEl = document.getElementById('countdown')
-let a = setInterval(updateCountdown, 1000);
+function TIMER() {
+    // 20 MINUTE WORKING TIME Function:
+    const startingMinutes = 1;
+    let time = startingMinutes * 60; // 1 minute * 60 seconds
+    const countdownEl = document.getElementById('countdown')
+    let a = setInterval(updateCountdown, 1000);
 
-function updateCountdown() {
-    const minutes = Math.floor(time/60);
-    let seconds = time % 60;
+    function updateCountdown() {
+        const minutes = Math.floor(time/60);
+        let seconds = time % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    countdownEl.innerHTML = `${minutes}:${seconds}`;
-    time--;
-    if (countdownEl.innerHTML == '0:00') {
-        clearInterval(a)
-        chrome.notifications.create(options, callback);
-        
-        // BREAKTIME FUNCTION HERE: 
-        const startingSeconds = 20;
-        let time = startingSeconds; // 1 minute * 60 seconds
-        const countdownEl = document.getElementById('countdown')
-        let b = setInterval(updateBreaktime, 1000);
+        countdownEl.innerHTML = `${minutes}:${seconds}`;
+        time--;
+        if (countdownEl.innerHTML == '0:00') {
+            clearInterval(a)
+            chrome.notifications.create(options, callback);
+            
+            // BREAKTIME FUNCTION HERE: 
+            const startingSeconds = 20;
+            let time = startingSeconds; // 1 minute * 60 seconds
+            const countdownEl = document.getElementById('countdown')
+            let b = setInterval(updateBreaktime, 1000);
 
-        function updateBreaktime() {
-            const minutes = Math.floor(time/60);
-            let seconds = time % 60;
+            function updateBreaktime() {
+                const minutes = Math.floor(time/60);
+                let seconds = time % 60;
 
-            seconds = seconds < 10 ? '0' + seconds : seconds;
+                seconds = seconds < 10 ? '0' + seconds : seconds;
 
-            countdownEl.innerHTML = `${minutes}:${seconds}`;
-            time--;
-            if (countdownEl.innerHTML == '0:00') {
-                clearInterval(b)
-                chrome.notifications.create(options, callback);
-            }   
+                countdownEl.innerHTML = `${minutes}:${seconds}`;
+                time--;
+                if (countdownEl.innerHTML == '0:00') {
+                    clearInterval(b)
+                    chrome.notifications.create(options, callback);
+                }   
+            }
         }
     }
 }
+
+//Calling the Function
+TIMER()
