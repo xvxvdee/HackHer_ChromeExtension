ddd531163121ebdda30ff762720a928f5b8ddde7
kaminese <56663339+kaminese@users.noreply.github.com>
2020-11-07 17:54:07 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,30 +1,21 @@
 var options = {
     type: "basic",
-    title: "My First Notif",
+    title: "Stay Healthy",
     message: "Time to Take a Break",
     iconUrl: "logo2.png"
 };
 
-/* 
-chrome.notifications.create(options, callback);
-console.log('POP');
-
 function callback(){
     console.log('Popup Done!');
-    }
-*/
+}
 
-
-//Timer Function: 
+// 20 MINUTE WORKING TIME Function:
 const startingMinutes = 1;
-let time = startingMinutes * 60;
-
+let time = startingMinutes * 60; // 1 minute * 60 seconds
 const countdownEl = document.getElementById('countdown')
-
 let a = setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
-    
     const minutes = Math.floor(time/60);
     let seconds = time % 60;
 
@@ -35,10 +26,25 @@ function updateCountdown() {
     if (countdownEl.innerHTML == '0:00') {
         clearInterval(a)
         chrome.notifications.create(options, callback);
-            console.log('POP');
+        
+        // BREAKTIME FUNCTION HERE: 
+        const startingSeconds = 20;
+        let time = startingSeconds; // 1 minute * 60 seconds
+        const countdownEl = document.getElementById('countdown')
+        let b = setInterval(updateBreaktime, 1000);
 
-        function callback(){
-            console.log('Popup Done!');
+        function updateBreaktime() {
+            const minutes = Math.floor(time/60);
+            let seconds = time % 60;
+
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+
+            countdownEl.innerHTML = `${minutes}:${seconds}`;
+            time--;
+            if (countdownEl.innerHTML == '0:00') {
+                clearInterval(b)
+                chrome.notifications.create(options, callback);
+            }   
         }
     }
 }
